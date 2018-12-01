298c43cb13a7bacd92fd7215f241233186345290
Luan <luann.porfirio@gmail.com>
2018-12-01 20:13:38 -0200

    adding README and extracting price from brickset website

---
+++ README.md
@@ -0,0 +1,8 @@
+Crawling data from [BrickSet](https://brickset.com/sets/year-2018) using
+[Scrapy](https://scrapy.org/) and Python3.
+
+Run: '''scrapy runspider scraper.py'''
+
+
+
+Tutorial obtained from [DigitalOcean](https://www.digitalocean.com/community/tutorials/como-fazer-crawling-em-uma-pagina-web-com-scrapy-e-python-3-pt)
--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 import scrapy
-
+import pdb
 class BrickSetSpider(scrapy.Spider):
     name = "brickset_spider"
     start_urls = ['http://brickset.com/sets/year-2018']
@@ -10,16 +10,18 @@ class BrickSetSpider(scrapy.Spider):
             NAME_SELECTOR = 'h1 ::text'
             PIECES_SELECTOR = './/dl[dt/text() = "Pieces"]/dd/a/text()'
             MINIFIGS_SELECTOR = './/dl[dt/text() = "Minifigs"]/dd[2]/a/text()'
+            PRICE_SELECTOR = './/dl[dt/text() = "RRP"]/dd/text()'
             IMAGE_SELECTOR = 'img ::attr(src)'
             yield {
                 'name' : brickset.css(NAME_SELECTOR).extract_first(),
                 'pieces' : brickset.xpath(PIECES_SELECTOR).extract_first(),
                 'minifigs' : brickset.xpath(MINIFIGS_SELECTOR).extract_first(),
+                'price' : brickset.xpath(PRICE_SELECTOR).extract_first(),
                 'image' : brickset.css(IMAGE_SELECTOR).extract_first()
             }
-        
         NEXT_PAGE_SELECTOR = '.next a ::attr(href)'
         next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
         if next_page:
             yield scrapy.Request(response.urljoin(next_page),
                 callback=self.parse)
+
